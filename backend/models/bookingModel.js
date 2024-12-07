df0ca57bdac6ef2922a39bb3e1236e2c4cd5affc
NguyenDucHuy <nguyenduchuy2004ndh@gmail.com>
2024-12-07 17:43:57 +0700

    add

--- backend/models/bookingModel.js
+++ backend/models/bookingModel.js
@@ -1,17 +1,4 @@
-// backend/src/models/bookingModel.js
-// class Booking {
-//   constructor(data) {
-//     this.id = data.id;
-//     this.userId = data.userId;
-//     this.flightId = data.flightId;
-//     this.passengerName = data.passengerName;
-//     this.passengerEmail = data.passengerEmail;
-//     this.bookingDate = data.bookingDate;
-//     this.status = data.status;
-//   }
-// }
-
-// export default Booking;
+//backend/src/models/bookingModel.js
 class Booking {
   constructor(data) {
     this.id = data.id;
@@ -19,10 +6,7 @@ class Booking {
     this.flightId = data.flightId;
     this.passengerName = data.passengerName;
     this.passengerEmail = data.passengerEmail;
-    this.bookingClass = data.bookingClass;
-    this.seatNumber = data.seatNumber;
     this.bookingDate = data.bookingDate;
-    this.totalPrice = data.totalPrice;
     this.status = data.status;
   }
 }
--- backend/models/flightModel.js
+++ backend/models/flightModel.js
@@ -1,38 +1,14 @@
 // backend/src/models/flightModel.js
-// class Flight {
-//   constructor(data) {
-//     this.id = data.id;
-//     this.flightNumber = data.flightNumber;
-//     this.from = data.from;
-//     this.to = data.to;
-//     this.departureTime = data.departureTime;
-//     this.arrivalTime = data.arrivalTime;
-//     this.price = data.price;
-//     this.availableSeats = data.availableSeats;
-//   }
-// }
-
-// export default Flight;
 class Flight {
   constructor(data) {
     this.id = data.id;
     this.flightNumber = data.flightNumber;
-    this.from = data.departureAirport;
-    this.to = data.arrivalAirport;
+    this.from = data.from;
+    this.to = data.to;
     this.departureTime = data.departureTime;
     this.arrivalTime = data.arrivalTime;
-    this.totalSeats = data.totalSeats;
+    this.price = data.price;
     this.availableSeats = data.availableSeats;
-    this.classTypes = {
-      economy: {
-        price: data.classTypes.economy.price,
-        availableSeats: data.classTypes.economy.availableSeats,
-      },
-      business: {
-        price: data.classTypes.business.price,
-        availableSeats: data.classTypes.business.availableSeats,
-      },
-    };
   }
 }
 
